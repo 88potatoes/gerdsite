f99af552086dbef44060b31de3ddd30de451f317
Eric Lang <94770511+88potatoes@users.noreply.github.com>
2024-03-02 14:08:45 +1100

    plantcomponent

---
+++ src/components/PlantComponent.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="plantcomponent">
+    <div>
+      <strong>{{ name }}</strong>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+    .plantcomponent {
+        border: 2px solid black;
+        width: 200px;
+        height: 200px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
